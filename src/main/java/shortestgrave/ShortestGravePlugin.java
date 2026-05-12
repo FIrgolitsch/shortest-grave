d614cff0d3b6acd588095803dc1ee5601b8aca26
Frans Irgolitsch <f.irgolitsch@outlook.com>
2026-05-12 15:17:13 -0400

    Added comments, ensured colour is only send to shortest path when it matters.

--- src/main/java/shortestgrave/ShortestGravePlugin.java
+++ src/main/java/shortestgrave/ShortestGravePlugin.java
@@ -52,6 +52,7 @@ public class ShortestGravePlugin extends Plugin
 		Actor actor = actorDeath.getActor();
 		if (actor instanceof Player && actor.equals(currentPlayer))
 		{
+			// Store death location and start countdown
 			pendingGraveLocation = this.currentPlayer.getWorldLocation();
 			ticksUntilPath = this.config.pathDelay();
 		}
@@ -64,6 +65,8 @@ public class ShortestGravePlugin extends Plugin
 		{
 			if (this.currentPlayer == null)
 			{
+				// Keep trying to set the current player until sucess.
+				// localPlayer is not set immediatly after GameSate switches to LOGGED_IN.
 				clientThread.invokeLater(() ->
 				{
 					this.currentPlayer = this.client.getLocalPlayer();
@@ -76,10 +79,14 @@ public class ShortestGravePlugin extends Plugin
 	@Subscribe
 	public void onChatMessage(ChatMessage chatMessage)
 	{
+		// Only set gravestone spawned if the message comes form the game and there is still a path to be planned.
+		// Prevents planning another route after the gravestone is emptied.
 		if (chatMessage.getMessage().toLowerCase().contains("gravestone") &&
 			this.pendingGraveLocation != null &&
 			chatMessage.getType() == ChatMessageType.GAMEMESSAGE)
 		{
+			// Must be set before the countdown has reached zero.
+			// See pathDelay in config
 			this.gravestoneSpawned = true;
 		}
 	}
@@ -88,11 +95,13 @@ public class ShortestGravePlugin extends Plugin
 	@Subscribe
 	public void onGameTick(GameTick gameTick)
 	{
+		// Wait until player has respawned.
 		if (this.ticksUntilPath > 0)
 		{
 			this.ticksUntilPath--;
 			if (this.ticksUntilPath == 0)
 			{
+				// Only spawn when the gravestone has spawned or the user has overriden the behaviour.
 				if (this.config.alwaysPath() || this.gravestoneSpawned)
 				{
 					this.ticksUntilPath = -1;
@@ -108,20 +117,37 @@ public class ShortestGravePlugin extends Plugin
 		return configManager.getConfig(ShortestGraveConfig.class);
 	}
 
+	/**
+	 * Delayed function after death.
+	 *
+	 * @param graveLocation The location where the player has died.
+	 */
 	private void afterDeath(WorldPoint graveLocation)
 	{
 		WorldPoint respawnLocation = this.client.getLocalPlayer().getWorldLocation();
 		sendShortestPathMessage(respawnLocation, graveLocation);
 	}
 
+	/**
+	 * Send a path to Shortest Path with a custom color.
+	 *
+	 * @param startPoint The start point of the path
+	 * @param endPoint   The end point of the path (the gravestone is this case)
+	 */
 	private void sendShortestPathMessage(WorldPoint startPoint, WorldPoint endPoint)
 	{
 		Map<String, Object> shortestPathMessage = new HashMap<>();
-		Map<String, Object> configOverride = new HashMap<>();
-		configOverride.put("colourPath", this.config.colourPath());
+
 		shortestPathMessage.put("start", startPoint);
 		shortestPathMessage.put("target", endPoint);
-		shortestPathMessage.put("config", configOverride);
+
+		// Don't bother to send a null object to shortest path when no colour is set in the config.
+		if (this.config.colourPath() != null)
+		{
+			Map<String, Object> configOverride = new HashMap<>();
+			configOverride.put("colourPath", this.config.colourPath());
+			shortestPathMessage.put("config", configOverride);
+		}
 
 		eventBus.post(new PluginMessage("shortestpath", "path", shortestPathMessage));
 		this.pendingGraveLocation = null;
